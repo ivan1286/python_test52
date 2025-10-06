a6296006b00dbd986eee04e6c234c86d019a291d
Ivanko Ivan <ivanko.ivan@lll.kpi.ua>
2025-10-06 17:36:40 +0300

    add constructor edited add_meals method

--- trackers/nutritioncontrol.py
+++ trackers/nutritioncontrol.py
@@ -3,9 +3,17 @@ class Nutrition:
    consumed_calories = ""
    calories_remaining = ""
 
+
+def __init__(self, total_calories):
+    self.total_calories = total_calories
+    self.consumed_calories = 0
+    self.calories_remaining = total_calories
+    self.meals = []
+
 def add_meals(self,calories,meal_name):
     self.consumed_calories += calories
     self.calories_remaining -= calories
+    self.meals.append(f"{meal_name} - {calories}")
     print(f"{meal_name} - {calories} calories was added to the nutrition")
 
 def get_consumed_calories(self):
