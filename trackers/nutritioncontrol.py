0e07a1d3e515c92c3f13f004a06855fe70e95928
Ivanko Ivan <ivanko.ivan@lll.kpi.ua>
2025-10-06 17:21:47 +0300

    add get_consumed_calories

--- trackers/nutritioncontrol.py
+++ trackers/nutritioncontrol.py
@@ -8,6 +8,9 @@ def add_meals(self,calories,meal_name):
     self.calories_remaining -= calories
     print(f"{meal_name} - {calories} calories was added to the nutrition")
 
+def get_consumed_calories(self):
+    return self.consumed_calories
+
 def get_remaining_calories(self):
     return self.calories_remaining
 
